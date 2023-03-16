1da0b7a631d3956142f3abaffad5c0f8388fcc8d
Thanh98-bg <thanhbg4117@gmail.com>
2023-03-16 22:35:05 +0700

    lesson 16: Add Category class

---
+++ BanSach/BanSachWeb/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace BanSachWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreateDatetime { get; set; } = DateTime.Now;
+    }
+}
