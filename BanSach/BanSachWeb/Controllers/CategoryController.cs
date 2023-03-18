314ed62181e9632ca76e7abaaa694a842d5859ee
Thanh98-bg <thanhbg4117@gmail.com>
2023-03-18 23:14:01 +0700

    Lesson 27,28,29: Create Category View

--- BanSach/BanSachWeb/Controllers/CategoryController.cs
+++ BanSach/BanSachWeb/Controllers/CategoryController.cs
@@ -16,5 +16,22 @@ namespace BanSachWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("index");
+            }
+            return View(obj);
+        }
     }
 }
