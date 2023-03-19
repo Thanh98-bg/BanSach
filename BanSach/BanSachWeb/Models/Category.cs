3c610adc46a463ad46d2bd52ef972e50c2cfb324
Thanh98-bg <thanhbg4117@gmail.com>
2023-03-19 22:45:52 +0700

    Lesson 32: Custom display and variable value

--- BanSach/BanSachWeb/Models/Category.cs
+++ BanSach/BanSachWeb/Models/Category.cs
@@ -8,6 +8,8 @@ namespace BanSachWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [Display(Name="Display Order")]
+        [Range(1, 100, ErrorMessage = "Value for {0} must between {1} and {2}.")]
         public int DisplayOrder { get; set; }
         public DateTime CreateDatetime { get; set; } = DateTime.Now;
     }
