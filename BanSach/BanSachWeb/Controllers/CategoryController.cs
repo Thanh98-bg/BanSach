e701bfe4a7bda1fe65cf047c21823f9bc6e6c2e9
Thanh98-bg <thanhbg4117@gmail.com>
2023-03-19 23:34:34 +0700

    Lesson 33,34: Edit category

--- BanSach/BanSachWeb/Controllers/CategoryController.cs
+++ BanSach/BanSachWeb/Controllers/CategoryController.cs
@@ -38,5 +38,35 @@ namespace BanSachWeb.Controllers
             }
             return View(obj);
         }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.FirstOrDefault(x=>x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == null || obj.Name == "")
+            {
+                ModelState.AddModelError("CustomError", "The name must not be empty");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("index");
+            }
+            return View(obj);
+        }
     }
 }
