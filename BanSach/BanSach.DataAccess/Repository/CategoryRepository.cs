7334c51c56d70f0246bd67fcd0c1f69fbcde8ab8
Thanh98-bg <thanhbg4117@gmail.com>
2023-03-26 17:29:17 +0700

    Lesson 81: Add CategoryRepository class

---
+++ BanSach/BanSach.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using BanSach.DataAccess.Data;
+using BanSach.DataAccess.Repository.IRepository;
+using BanSach.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanSach.DataAccess.Repository
+{
+    internal class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            _db.Categories.Update(category);
+        }
+    }
+}
