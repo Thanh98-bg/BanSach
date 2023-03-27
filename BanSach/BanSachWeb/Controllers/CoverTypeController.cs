12696b094084cc61503717bf8ecb318fb6c4571e
Thanh98-bg <thanhbg4117@gmail.com>
2023-03-27 23:32:43 +0700

    Make Index action of CoverType

---
+++ BanSach/BanSachWeb/Controllers/CoverTypeController.cs
@@ -0,0 +1,14 @@
+﻿using BanSach.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BanSachWeb.Controllers
+{
+    public class CoverTypeController : Controller
+    {
+        public IActionResult Index()
+        {
+            IEnumerable<CoverType> covers = new List<CoverType>();
+            return View(covers);
+        }
+    }
+}
