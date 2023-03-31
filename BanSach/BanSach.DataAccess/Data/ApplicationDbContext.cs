64fe414480c4af4c6002c602622536edec41e2fd
Thanh98-bg <thanhbg4117@gmail.com>
2023-03-31 23:00:26 +0700

    Lesson 93: Create Product table

--- BanSach/BanSach.DataAccess/Data/ApplicationDbContext.cs
+++ BanSach/BanSach.DataAccess/Data/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace BanSach.DataAccess.Data
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
 
     }
 }
