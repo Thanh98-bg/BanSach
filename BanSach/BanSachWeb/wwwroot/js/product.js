d4eb25e547bc80ec0f3a0ef9ed1bc3f3a45607b5
Thanh98-bg <thanhbg4117@gmail.com>
2023-04-08 16:00:49 +0700

    Lesson 105: Display data on the table

---
+++ BanSach/BanSachWeb/wwwroot/js/product.js
@@ -0,0 +1,20 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDatatable();
+});
+
+function loadDatatable() {
+    dataTable = $('#tblData').dataTable(
+        {
+            "ajax": {
+                "url": "/admin/product/getall"
+            },
+            "columns": [
+                {'data': 'title', "width":"15%"},
+                {'data': 'isbn', "width":"15%"},
+                {'data': 'price50', "width":"15%"},
+                {'data': 'author', "width":"15%"}
+            ]
+        }
+    )
+}
