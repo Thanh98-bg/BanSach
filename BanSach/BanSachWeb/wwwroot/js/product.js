a55b6f00924a7ca5159005a0575c022b2d71bf6a
Thanh98-bg <thanhbg4117@gmail.com>
2023-04-18 23:13:38 +0700

    Lesson 107: Add Edit, Delete button

--- BanSach/BanSachWeb/wwwroot/js/product.js
+++ BanSach/BanSachWeb/wwwroot/js/product.js
@@ -14,7 +14,17 @@ function loadDatatable() {
                 {'data': 'isbn', "width":"15%"},
                 {'data': 'price50', "width":"15%"},
                 {'data': 'author', "width":"15%"},
-                { 'data': 'category.name', "width": "15%" }
+                { 'data': 'category.name', "width": "15%" },
+                {
+                    'data': 'id',
+                    'render': function (data) {
+                        return `<div>
+                        <a href="/Admin/Product/Upsert?id=${data}" class="btn btn-primary" > <i class="bi bi-pencil"></i></a>
+                        <a class="btn btn-primary"><i class="bi bi-trash3"></i></a>
+                        </div >`
+                    },
+                    "width": "15%"
+                }
             ]
         }
     )
