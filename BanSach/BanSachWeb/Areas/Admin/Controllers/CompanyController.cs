e2ba6a801aa636b4f389d3826927a61744b32c06
Thanh98-bg <thanhbg4117@gmail.com>
2023-05-09 23:26:46 +0700

    Create company

---
+++ BanSach/BanSachWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,42 @@
+﻿using BanSach.DataAccess.Repository.IRepository;
+using BanSach.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BanSachWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Company> companies = _unitOfWork.Company.GetAll();
+            return View(companies);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Company company) 
+        {
+            if (company == null)
+            {
+                ModelState.AddModelError("CustomError", "Input error!");
+            }
+            else if (ModelState.IsValid)
+            {
+                _unitOfWork.Company.Add(company);
+                _unitOfWork.Save();
+                TempData["Success"] = "Company create successfully";
+                return RedirectToAction("Index");
+            }
+            return View(company);
+        }
+    }
+}
