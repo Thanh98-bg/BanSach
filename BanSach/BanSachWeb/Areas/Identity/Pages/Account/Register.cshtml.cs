0eed64615d9887e5f483956e29c7dd2d7fea2778
Thanh98-bg <thanhbg4117@gmail.com>
2023-05-18 23:30:54 +0700

    Lesson 126: Add option property on register page

--- BanSach/BanSachWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BanSach/BanSachWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -19,6 +19,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using BanSach.Utility;
+using BanSach.Model;
 
 namespace BanSachWeb.Areas.Identity.Pages.Account
 {
@@ -100,6 +101,13 @@ namespace BanSachWeb.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+            [Required]
+            public string Name { get; set; }
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
         }
 
 
@@ -126,6 +134,12 @@ namespace BanSachWeb.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.Name = Input.Name;
+                user.PhoneNumber = Input.PhoneNumber;
+                user.City = Input.City;
+                user.StreetAddress = Input.StreetAddress;
+                user.PostalCode = Input.PostalCode;
+                user.State = Input.State;
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
@@ -164,11 +178,11 @@ namespace BanSachWeb.Areas.Identity.Pages.Account
             return Page();
         }
 
-        private IdentityUser CreateUser()
+        private ApplicationUser CreateUser()
         {
             try
             {
-                return Activator.CreateInstance<IdentityUser>();
+                return Activator.CreateInstance<ApplicationUser>();
             }
             catch
             {
