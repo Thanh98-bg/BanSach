8f5a9f85a0e42a9521459fb90a3ffee4f6b04c29
Thanh98-bg <thanhbg4117@gmail.com>
2023-05-27 17:07:48 +0700

    Lesson 136: Configure applicaiton cookie

--- BanSach/BanSachWeb/Program.cs
+++ BanSach/BanSachWeb/Program.cs
@@ -18,6 +18,12 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProvid
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
