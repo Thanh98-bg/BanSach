c228d5a8bf5db1dd82cd446eb1a1081752d487ea
Thanh98-bg <thanhbg4117@gmail.com>
2023-05-29 23:28:09 +0700

    Lesson 144: Add OrderHeader & OrderDetail to UnitOfWork

---
+++ BanSach/BanSach.DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,25 @@
+﻿using BanSach.DataAccess.Data;
+using BanSach.DataAccess.Repository.IRepository;
+using BanSach.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanSach.DataAccess.Repository
+{
+	public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+	{
+		private readonly ApplicationDbContext _db;
+		public OrderDetailRepository(ApplicationDbContext db) : base(db)
+		{
+			_db = db;
+		}
+
+		public void Update(OrderDetail orderDetail)
+		{
+			_db.OrderDetails.Update(orderDetail);
+		}
+	}
+}
--- BanSach/BanSach.DataAccess/Repository/UnitOfWork.cs
+++ BanSach/BanSach.DataAccess/Repository/UnitOfWork.cs
@@ -20,7 +20,10 @@ namespace BanSach.DataAccess.Repository
             Company = new CompanyRepository(db);
             ShoppingCart = new ShoppingCartRepository(db);
             ApplicationUser = new ApplicationUserRepository(db);
-        }
+			OrderHeader = new OrderHeaderRepository(db);
+            OrderDetail = new OrderDetailRepository(db);
+
+		}
         public ICategoryRepository Category { get; private set; }
 
         public ICoverTypeRepository CoverType { get; private set; }
@@ -30,6 +33,8 @@ namespace BanSach.DataAccess.Repository
         public ICompanyRepository Company { get; private set; }
         public IShoppingCartRepository ShoppingCart { get; private set; }
         public IApplicationUserRepository ApplicationUser { get; private set; }
+        public IOrderHeaderRepository OrderHeader { get; private set; }
+        public IOrderDetailRepository OrderDetail { get; private set; }
 
         public void Save()
         {
