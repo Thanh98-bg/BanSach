cae35619330e2d253f910600eeb020872e4c6a7e
Thanh98-bg <thanhbg4117@gmail.com>
2023-05-29 22:56:25 +0700

    Lesson 143: Create OrderHeader and OrderDetail tables

--- BanSach/BanSach.DataAccess/Data/ApplicationDbContext.cs
+++ BanSach/BanSach.DataAccess/Data/ApplicationDbContext.cs
@@ -21,6 +21,8 @@ namespace BanSach.DataAccess.Data
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
-    }
+	}
 }
